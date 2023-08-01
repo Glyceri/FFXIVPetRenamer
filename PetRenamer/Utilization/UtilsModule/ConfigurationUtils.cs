0fe2d90f2e6cd3c638cefd7d1ae5a67f2e65d644
Amber Kortier <AmberKortier@gmail.com>
2023-08-01 12:34:21 +0200

    Auto stash before merge of "007-PetsPerUser" and "008-LegacyHandling"

--- PetRenamer/Utilization/UtilsModule/ConfigurationUtils.cs
+++ PetRenamer/Utilization/UtilsModule/ConfigurationUtils.cs
@@ -1,4 +1,4 @@
-﻿using PetRenamer.Core.Handlers;
+using PetRenamer.Core.Handlers;
 using PetRenamer.Core.Serialization;
 using PetRenamer.Utilization.Attributes;
 using System;
@@ -10,6 +10,40 @@ namespace PetRenamer.Utilization.UtilsModule;
 [UtilsDeclarable]
 internal class ConfigurationUtils : UtilsRegistryType
 {
+    public void SetLocalNickname(int forPet, string nickname)
+    {
+        if (PluginLink.Configuration.serializableUsers!.Length == 0) return;
+
+        if (!PluginLink.Utils.Get<NicknameUtils>().ContainsLocal(forPet))
+        {
+            List<SerializableNickname> nicknames = PluginLink.Configuration.serializableUsers[0]!.nicknames.ToList();
+            nicknames.Add(new SerializableNickname(forPet, nickname));
+            PluginLink.Configuration.serializableUsers[0]!.nicknames = nicknames.ToArray();
+        }
+
+        SerializableNickname nick = PluginLink.Utils.Get<NicknameUtils>().GetLocalNickname(forPet);
+        if (nick != null)
+            nick.Name = nickname;
+
+        PluginLink.Configuration.Save();
+    }
+
+    public void RemoveLocalNickname(int forPet)
+    {
+        if (PluginLink.Configuration.serializableUsers!.Length == 0) return;
+
+        if (PluginLink.Utils.Get<NicknameUtils>().ContainsLocal(forPet))
+        {
+            List<SerializableNickname> nicknames = PluginLink.Configuration.serializableUsers[0]!.nicknames.ToList();
+            for (int i = nicknames.Count - 1; i >= 0; i--)
+                if (nicknames[i].ID == forPet)
+                    nicknames.RemoveAt(i);
+
+            PluginLink.Configuration.serializableUsers[0]!.nicknames = nicknames.ToArray();
+            PluginLink.Configuration.Save();
+        }
+    }
+
     [Obsolete]
     public void SetNickname(int forPet, string nickname)
     {
