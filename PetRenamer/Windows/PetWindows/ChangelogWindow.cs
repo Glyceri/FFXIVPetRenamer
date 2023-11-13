046c19dee30ee1dfc375957beec374d28fc223ea
Amber Kortier <AmberKortier@gmail.com>
2023-11-13 14:24:00 +0100

    Version Updated

--- PetRenamer/Windows/PetWindows/ChangelogWindow.cs
+++ PetRenamer/Windows/PetWindows/ChangelogWindow.cs
@@ -73,6 +73,7 @@ internal class ChangelogWindow : PetWindow
         { new CLS(new Version("1.4.3.3"), new CLE("General", "The plugin should work for people with a - in their name now!", "A warning will now be displayed when you enter a PVP area and the plugin disables itself.", "Fixed an IPC issue.")) },
         { new CLS(new Version("1.4.3.4"), new CLE("General", "The plugin will now save upon removing a user!", "Improved stability upon logging into an alt account.", "Code optimizations."), new CLE("UI", "The pet list will now reset upon relogging.", "Fixed an issue where the pet list wouldn't draw correctly under certain circumstances.")) },
         { new CLS(new Version("1.4.3.5"), new CLE("General", "Fixed stutter upon summoning a pet", "You can now give nicknames to pets turned into player characters again.\n(Gamers, I'm serious! If I get even a single report of this being abused again, I will disable the feature again for everybody!)", "Code optimizations.")) },
+        { new CLS(new Version("1.4.4.0"), new CLE("General", "Fixed an issue where the Pet Rename Window would sometimes not work.", "Mappy is now integrated into Pet Nicknames.")) },
     };
 
     const string starter = "  ◉   ";
