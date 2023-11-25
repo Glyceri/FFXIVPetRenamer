439bacb8343dd79941358a6e6e864d6ac013a192
Amber Kortier <AmberKortier@gmail.com>
2023-11-25 11:12:39 +0100

    Updated version!

--- PetRenamer/Windows/PetWindows/ChangelogWindow.cs
+++ PetRenamer/Windows/PetWindows/ChangelogWindow.cs
@@ -78,6 +78,7 @@ internal class ChangelogWindow : PetWindow
         { new CLS(new Version("1.4.4.2"), new CLE("General", "Massive performance improvements!")) },
         { new CLS(new Version("1.4.4.3"), new CLE("General", "Fixed log spam that could occur.", "Changed to Mappy IPC to be compatible with the new update. (We forgive, but never forget!)", "Fixed an issue where upon switching alts another log would spam.")) },
         { new CLS(new Version("1.4.4.4"), new CLE("General", "Added the ability to reorder the Petlist.")) },
+        { new CLS(new Version("1.4.4.5"), new CLE("General", "This plugin now works for users with a new save file!")) },
     };
 
     const string starter = "  ◉   ";
