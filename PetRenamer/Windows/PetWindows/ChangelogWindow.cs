45c0982b8d6f0267a0b2f4e667d613fae2c4edb1
Amber Kortier <AmberKortier@gmail.com>
2024-01-10 01:50:26 +0100

    Version updated

--- PetRenamer/Windows/PetWindows/ChangelogWindow.cs
+++ PetRenamer/Windows/PetWindows/ChangelogWindow.cs
@@ -85,6 +85,7 @@ internal class ChangelogWindow : PetWindow
         { new CLS(new Version("1.4.5.3"), new CLE("UI", "Temporarily adds fireworks on the toolbar (You can now also select any of the older toolbar animations).")) },
         { new CLS(new Version("1.4.5.4"), new CLE("UI", "The setting automatically Switch Petmode should now work again.")) },
         { new CLS(new Version("1.4.6.0"), new CLE("General", "Giving a Battle Pet command or them leaving the battle will now properly show their nicknames in chat.", "Tentatively fixed an issue to do with soft skeletons (/petmirage)."), new CLE("UI", "Fireworks in the toolbar have been removed from the event.")) },
+        { new CLS(new Version("1.4.6.1"), new CLE("General", "Names should now display properly in German.")) },
     };
 
     const string starter = "  ◉   ";
