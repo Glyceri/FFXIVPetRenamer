5877bbf3349909bdbd437aae7469791d948e68ce
Amber Kortier <AmberKortier@gmail.com>
2024-07-02 23:21:29 +0200

    Pet dev!

--- PetRenamer/Windows/PetWindows/DeveloperWindow.cs
+++ PetRenamer/Windows/PetWindows/DeveloperWindow.cs
@@ -2,6 +2,7 @@
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using PetRenamer.Core.Handlers;
+using PetRenamer.Core.PettableUserSystem;
 using PetRenamer.Core.PettableUserSystem.Enums;
 using PetRenamer.Core.Serialization;
 using PetRenamer.Windows.Attributes;
@@ -22,15 +23,15 @@ internal unsafe class DeveloperWindow : PetWindow
    
     public override void OnDraw()
     {
-        if (Button("Add All"))
+        if (Button("Add All##11111"))
         {
-            for (int i = 0; i < PluginHandlers.ObjectTable.Length - 1; i += 2)
+            for (int i = 0; i < PluginHandlers.ObjectTable.Length - 1; i ++)
             {
                 IGameObject? gObj = PluginHandlers.ObjectTable[i];
                 if (gObj == null) continue;
                 if (gObj.ObjectKind != Dalamud.Game.ClientState.Objects.Enums.ObjectKind.Player) continue;
                 int minionTarget = i + 1;
-                IGameObject? companion = PluginHandlers.ObjectTable[i];
+                IGameObject? companion = PluginHandlers.ObjectTable[minionTarget];
                 if (companion == null) continue;
                 if (companion.ObjectKind != Dalamud.Game.ClientState.Objects.Enums.ObjectKind.Companion) continue;
                 Companion* cPointer = (Companion*)companion.Address;
@@ -45,11 +46,23 @@ internal unsafe class DeveloperWindow : PetWindow
                         gObj.Name.ToString(),
                         (ushort)PluginHandlers.ClientState.LocalPlayer!.HomeWorld.Id), UserDeclareType.Add, false, false);
             }
+            PluginLink.Configuration.Save();
         }
 
         if (Button("Remove All"))
         {
-
+            for (int i = PluginLink.PettableUserHandler.Users.Count - 1; i >= 0; i--)
+            {
+                PettableUser user = PluginLink.PettableUserHandler.Users[i];
+                SerializableUserV3 serializableUser = user.SerializableUser;
+                for (int f = 0; f < user.SerializableUser.length; f++)
+                {
+                    if (serializableUser[i].Name != "[TESTNAME]") continue;
+                    PluginLink.PettableUserHandler.DeclareUser(user.SerializableUser, UserDeclareType.Remove, false, false);
+                    break;
+                }
+            }
+            PluginLink.Configuration.Save();
         }
     }
 }
