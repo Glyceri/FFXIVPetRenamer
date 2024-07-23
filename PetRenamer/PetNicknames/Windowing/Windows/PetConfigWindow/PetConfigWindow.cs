1703668a138e167025134db2a2f75442b90b19ad
Amber Kortier <AmberKortier@gmail.com>
2024-07-23 22:51:35 +0200

    Language selector

--- PetRenamer/PetNicknames/Windowing/Windows/PetConfigWindow/PetConfigWindow.cs
+++ PetRenamer/PetNicknames/Windowing/Windows/PetConfigWindow/PetConfigWindow.cs
@@ -70,6 +70,7 @@ internal class PetConfigWindow : PetWindow
 
         GeneralSettingsNode.ContentNode.ChildNodes = [
              new ToggleConfig(in Configuration, Translator.GetLine("Config.ProfilePictures"), Configuration.downloadProfilePictures, (value) => Configuration.downloadProfilePictures = value),
+             new LanguageSettingsBar(in Configuration),
         ];
 
         UISettingsNode.ContentNode.ChildNodes = [
