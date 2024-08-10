18a42046abb4b0c73ccdab4ba44d20d347227d74
Amber <AmberKortier@gmail.com>
2024-08-10 18:38:57 +0200

    Cleared unused code

--- PetRenamer/PetNicknames/PettableUsers/PettableIslandUser.cs
+++ PetRenamer/PetNicknames/PettableUsers/PettableIslandUser.cs
@@ -7,7 +7,6 @@ using PetRenamer.PetNicknames.PettableUsers.Interfaces;
 using PetRenamer.PetNicknames.Services.Interface;
 using PetRenamer.PetNicknames.Services.ServiceWrappers.Interfaces;
 using System.Collections.Generic;
-using static PetRenamer.PetNicknames.PettableUsers.Interfaces.IPettableUser;
 
 namespace PetRenamer.PetNicknames.PettableUsers;
 
@@ -39,13 +38,9 @@ internal unsafe class PettableIslandUser : IIslandUser
         Name = entry.Name;
         ContentID = entry.ContentID;
         Homeworld = entry.Homeworld;
-
     }
 
-    public void Set(Pointer<BattleChara> pointer)
-    {
-        Reset();
-    }
+    public void Set(Pointer<BattleChara> pointer) => Reset();
 
     public void CalculateBattlepets(ref List<Pointer<BattleChara>> pets)
     {
@@ -118,39 +113,11 @@ internal unsafe class PettableIslandUser : IIslandUser
         return null;
     }
 
-    public IPettablePet? GetYoungestPet(IPettableUser.PetFilter filter = IPettableUser.PetFilter.None)
-    {
-        ulong lastLifetime = ulong.MaxValue;
-        IPettablePet? lastPet = null;
-        if (!IsActive) return null;
-        int petCount = PettablePets.Count;
-        for (int i = 0; i < petCount; i++)
-        {
-            IPettablePet pPet = PettablePets[i];
-            if (filter != PetFilter.None)
-            {
-                if (filter != PetFilter.Minion && pPet is PettableCompanion) continue;
-                if (filter != PetFilter.BattlePet && filter != PetFilter.Chocobo && pPet is PettableBattlePet) continue;
-                if (filter == PetFilter.BattlePet && !PetServices.PetSheets.IsValidBattlePet(pPet.SkeletonID)) continue;
-                if (filter == PetFilter.Chocobo && PetServices.PetSheets.IsValidBattlePet(pPet.SkeletonID)) continue;
-            }
-
-            if (pPet.Lifetime < lastLifetime)
-            {
-                lastLifetime = pPet.Lifetime;
-                lastPet = pPet;
-            }
-        }
-        return lastPet;
-    }
 
     public string? GetCustomName(IPetSheetData sheetData) => DataBaseEntry.GetName(sheetData.Model);
 
     public void OnLastCastChanged(uint cast) { } // Unused
     public void RefreshCast() { } // Unused
-
-    public void Dispose()
-    {
-
-    }
+    public IPettablePet? GetYoungestPet(IPettableUser.PetFilter filter = IPettableUser.PetFilter.None) => null; // Unused
+    public void Dispose() { } // Unused
 }
